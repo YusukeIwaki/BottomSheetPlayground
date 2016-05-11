8ed87bc108e7cf5e002fd7eef4318214b8213387
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2016-05-12 02:46:16 +0900

    雑にBottomSheetを使ってメッセージフォームを作ってみた

--- app/src/main/java/jp/co/crowdworks/bottomsheetplayground/ScrollingActivity.java
+++ app/src/main/java/jp/co/crowdworks/bottomsheetplayground/ScrollingActivity.java
@@ -1,13 +1,14 @@
 package jp.co.crowdworks.bottomsheetplayground;
 
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -18,12 +19,36 @@ public class ScrollingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final BottomSheetBehavior<NestedScrollView> bottomSheet = BottomSheetBehavior.from((NestedScrollView) findViewById(R.id.bottom_sheet));
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                bottomSheet.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                bottomSheet.setHideable(false);
+                fab.hide();
+            }
+        });
+
+        bottomSheet.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        findViewById(R.id.btn_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fab.show();
+                bottomSheet.setHideable(true);
+                bottomSheet.setState(BottomSheetBehavior.STATE_HIDDEN);
+            }
+        });
+
+        findViewById(R.id.btn_ok).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fab.show();
+                bottomSheet.setHideable(true);
+                bottomSheet.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+                //ここに送信処理を書く
             }
         });
     }
